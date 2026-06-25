5c909ab45dd42252e790bcddbb5ad97ca582f7c1
YREN37 <YREN37@volvocars.com>
2026-06-25 20:08:16 +0800

    简化 Jswitch 公式，移除对 macOS ARM 架构的条件判断

--- Formula/jswitch.rb
+++ Formula/jswitch.rb
@@ -4,15 +4,8 @@ class Jswitch < Formula
   version "0.1.0"
   license any_of: ["MIT", "Apache-2.0"]
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/m1ggle/jswitch/releases/download/v0.1.0/jswitch-aarch64-apple-darwin.tar.gz"
-      sha256 "97e72926dd4c59e18959bb82b8d97c1bbeb44b8459cfae5b76b624fc790c0b02"
-    else
-      url "https://github.com/m1ggle/jswitch/releases/download/v0.1.0/jswitch-x86_64-apple-darwin.tar.gz"
-      sha256 "97e72926dd4c59e18959bb82b8d97c1bbeb44b8459cfae5b76b624fc790c0b02"
-    end
-  end
+  url "https://github.com/m1ggle/jswitch/releases/download/v0.1.0/jswitch-x86_64-apple-darwin.tar.gz"
+  sha256 "97e72926dd4c59e18959bb82b8d97c1bbeb44b8459cfae5b76b624fc790c0b02"
 
   def install
     bin.install "jswitch"
