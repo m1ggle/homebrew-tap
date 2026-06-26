75606ec59772fe8989bf05d4fe623022022e9d15
YREN37 <YREN37@volvocars.com>
2026-06-26 16:51:59 +0800

    更新 Jswitch 公式至 0.1.1，更新下载链接和 SHA256 校验和

--- Formula/jswitch.rb
+++ Formula/jswitch.rb
@@ -4,8 +4,8 @@ class Jswitch < Formula
   version "0.1.1"
   license any_of: ["MIT", "Apache-2.0"]
 
-  url "https://github.com/m1ggle/jswitch/releases/download/v0.1.0/jswitch-x86_64-apple-darwin.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/m1ggle/jswitch/releases/download/v0.1.1/jswitch-x86_64-apple-darwin.tar.gz"
+  sha256 "84f3cc524ea36ca051ba6d2659b8c4a197eb5f2d387ebf6395aa25f82fcb33a4"
 
   def install
     bin.install "jswitch"
